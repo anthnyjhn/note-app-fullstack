2fc7ca8b49ff24e58b869dceaa417f34ac529c31
anthnyjhn <johncaronongan17@gmail.com>
2024-04-20 20:26:00 +1000

    added CRUD endpoint

--- server/routes/api.router.js
+++ server/routes/api.router.js
@@ -23,4 +23,16 @@ router.get("/list", (req, res) => {
   });
 });
 
+router.post('/create', (req,res) => {
+
+})
+
+router.put('/update', (req,res)=> {
+
+})
+
+router.delete('/delete', (req,res)=> {
+  
+})
+
 module.exports = router;
